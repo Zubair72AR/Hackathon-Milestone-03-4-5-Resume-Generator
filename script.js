e6f39464bec21d4aca57e9ed4af14dda9c99176b
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-13 20:18:13 +0500

    Updated

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 "use strict";
 // Add+ More Input for Form
+let delLanguageBtn = document.querySelector("#del-Language-Btn");
+let inTheLanguageSec = document.querySelector("#language-Sec");
+let beforeAddLanBtn = document.querySelector("#add-Language-Btn");
+let delAddBtn = 1;
 function addLanguageButton() {
+    // Creating HTML Element Input for Adding More Languages
     let addLanBtn = document.createElement("input");
+    // New Attributes for Created Input
     addLanBtn.setAttribute("type", "text");
-    let inTheLanguageSec = document.querySelector("#language-Sec");
-    let beforeAddLanBtn = document.querySelector("#add-Language-Btn");
+    addLanBtn.setAttribute("id", "language-Input");
+    addLanBtn.classList.add("form-input");
+    addLanBtn.setAttribute("placeholder", "Enter Here");
+    addLanBtn.setAttribute("value", "");
+    addLanBtn.setAttribute("required", "");
+    // Inserting Input
     inTheLanguageSec.insertBefore(addLanBtn, beforeAddLanBtn);
+    // Increment for Managing Delete Button
+    delAddBtn++;
+    // will Hide Delete Button
+    if (delAddBtn == 2) {
+        delLanguageBtn.style.display = "inline-block";
+    }
 }
-/*
-
-type="text"
-              name="language-Input"
-              id="language-Input"
-              class="form-input"
-              placeholder="Enter Here"
-              value=""
-              required
-
-              */
+// Deleting inputs if user created more then required
+function delLanguageButton() { }
--- script.ts
+++ script.ts
@@ -1,25 +1,30 @@
 // Add+ More Input for Form
+let delLanguageBtn = document.querySelector("#del-Language-Btn") as HTMLElement;
+let inTheLanguageSec = document.querySelector("#language-Sec") as HTMLElement;
+let beforeAddLanBtn = document.querySelector(
+  "#add-Language-Btn"
+) as HTMLElement;
+let delAddBtn = 1;
 
 function addLanguageButton() {
+  // Creating HTML Element Input for Adding More Languages
   let addLanBtn = document.createElement("input");
+  // New Attributes for Created Input
   addLanBtn.setAttribute("type", "text");
-
-  let inTheLanguageSec = document.querySelector("#language-Sec") as HTMLElement;
-  let beforeAddLanBtn = document.querySelector(
-    "#add-Language-Btn"
-  ) as HTMLElement;
-
+  addLanBtn.setAttribute("id", "language-Input");
+  addLanBtn.classList.add("form-input");
+  addLanBtn.setAttribute("placeholder", "Enter Here");
+  addLanBtn.setAttribute("value", "");
+  addLanBtn.setAttribute("required", "");
+  // Inserting Input
   inTheLanguageSec.insertBefore(addLanBtn, beforeAddLanBtn);
+  // Increment for Managing Delete Button
+  delAddBtn++;
+  // will Hide Delete Button
+  if (delAddBtn == 2) {
+    delLanguageBtn.style.display = "inline-block";
+  }
 }
 
-/*
-
-type="text"
-              name="language-Input"
-              id="language-Input"
-              class="form-input"
-              placeholder="Enter Here"
-              value=""
-              required
-
-              */
+// Deleting inputs if user created more then required
+function delLanguageButton() {}
