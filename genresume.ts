93989413a020b7bbb28d1fd6116874888692caaa
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-14 19:52:57 +0500

    updated

---
+++ genresume.ts
@@ -0,0 +1,119 @@
+function generateResume(){
+   
+    <main class="generate-Resume">
+      <!-- Resume Left Portion for About Me, Personal Info, Languages ETC. -->
+      <!-- Name & Designation for -->
+      <div class="section-Name">
+        <h1 class="name">Zubair Ahmed</h1>
+        <h3 class="designation">Web & Graphics Designer</h3>
+      </div>
+      <section class="left-Portion">
+        <!-- Profile Image -->
+        <img src="Porfolio Image.jpg" alt="Profile Image" />
+
+        <!-- Name & Designation for Media -->
+        <div class="section-NameMedia">
+          <h1 class="name">Zubair Ahmed</h1>
+          <h3 class="designation">Web & Graphics Designer</h3>
+        </div>
+
+        <!-- About Me -->
+        <h4 class="heading-AboutMe">About Me</h4>
+        <p class="para-AboutMe">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+          exercitationem, ipsam sit, illo aliquam officia earum autem ad
+          nesciunt rem, sed necessitatibus beatae soluta nobis veritatis.
+          Voluptas repudiandae harum repellendus!
+        </p>
+
+        <!-- Contact Info -->
+        <div class="contact-Sec">
+          <div class="contact-Info">
+            <div class="info-Icon"><i class="fa-solid fa-phone"></i></div>
+            <p class="info-Text">+92 333 123 4567</p>
+          </div>
+
+          <div class="contact-Info">
+            <div class="info-Icon"><i class="fa-solid fa-envelope"></i></div>
+            <p class="info-Text">youremail123@gmail.com</p>
+          </div>
+
+          <div class="contact-Info">
+            <div class="info-Icon">
+              <i class="fa-solid fa-location-dot"></i>
+            </div>
+            <p class="info-Text">Karachi, Sindh - Pakistan</p>
+          </div>
+        </div>
+
+        <!-- Languages Known -->
+        <div class="languages-Sec">
+          <h4 class="heading-Language">Languages</h4>
+        </div>
+        <div class="languages-List">
+          <ul class="list">
+            <li>English</li>
+            <li>Urdu</li>
+            <li>Arabic</li>
+          </ul>
+        </div>
+
+        <!-- Expertise Known -->
+        <div class="expertise-Sec">
+          <h4 class="heading-Expertise">Expertise</h4>
+        </div>
+        <div class="expertise-list">
+          <ul class="list">
+            <li>Management Skills</li>
+            <li>Creativity</li>
+            <li>Digital Marketing</li>
+            <li>Leadership</li>
+          </ul>
+        </div>
+
+        <!-- Left Portions Ends Here -->
+      </section>
+      <!-- Resume Right Portion for Experience, Education & Skills -->
+      <section class="right-Portion">
+        <!-- Experience Section -->
+        <div class="section-Experience">
+          <h3 class="heading-Experience">Experience</h3>
+        </div>
+        <div class="add-Sec-Experience">
+          <h5 class="title-Experience">Enter your position title here</h5>
+          <p class="comp-Experience">Company Name / Jan 2020 - Present</p>
+          <p class="para-Experience">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat
+            aperiam facere repellendus dolor aliquam sint placeat, reiciendis
+            rerum expedita! Laborum quos quod pariatur eligendi deserunt, nemo
+            nisi dolores excepturi dicta.
+          </p>
+        </div>
+
+        <!-- Education Section -->
+        <div class="section-Education">
+          <h3 class="heading-Education">Education</h3>
+        </div>
+        <div class="add-Sec-Education">
+          <h5 class="institute-Education">University of Karachi</h5>
+          <p class="degree-Education">Master of Business Management</p>
+          <p class="year-Education">Jan 2020 - Dec 2023</p>
+        </div>
+
+        <!-- Skills Section -->
+        <div class="section-Skills">
+          <h3 class="heading-Skills">Skills</h3>
+        </div>
+
+        <div class="skills-List-Sec">
+          <ul class="skills-List">
+            <li>Type/JavaScript</li>
+            <li>Next js</li>
+            <li>HTML CSS</li>
+          </ul>
+        </div>
+        <!-- Right Portions Ends Here -->
+      </section>
+    </main>
+
+}
