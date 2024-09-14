970d632330172a66f39b0ffe223420bd6c323006
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-13 18:49:10 -0700

    updated

--- script.ts
+++ script.ts
@@ -1,30 +1,69 @@
-// Add+ More Input for Form
-let delLanguageBtn = document.querySelector("#del-Language-Btn") as HTMLElement;
-let inTheLanguageSec = document.querySelector("#language-Sec") as HTMLElement;
-let beforeAddLanBtn = document.querySelector(
-  "#add-Language-Btn"
-) as HTMLElement;
-let delAddBtn = 1;
+// Add+ More Input for Languages
+let langDiv = document.querySelector("#lang-Div") as HTMLElement;
+let delLangBtn = document.querySelector("#del-Lang-Btn") as HTMLElement;
+let delAddBtnLang = 0;
 
-function addLanguageButton() {
-  // Creating HTML Element Input for Adding More Languages
+function addLanguageBtn() {
+  // Creating HTML Element
   let addLanBtn = document.createElement("input");
-  // New Attributes for Created Input
+  // New Attributes for Created Element
   addLanBtn.setAttribute("type", "text");
-  addLanBtn.setAttribute("id", `language-Input-${delAddBtn}`);
+  addLanBtn.setAttribute("id", "language-Input");
   addLanBtn.classList.add("form-input");
   addLanBtn.setAttribute("placeholder", "Enter Here");
   addLanBtn.setAttribute("value", "");
   addLanBtn.setAttribute("required", "");
   // Inserting Input
-  inTheLanguageSec.insertBefore(addLanBtn, beforeAddLanBtn);
+  langDiv.append(addLanBtn);
   // Increment for Managing Delete Button
-  delAddBtn++;
-  // will Hide Delete Button
-  if (delAddBtn == 2) {
-    delLanguageBtn.style.display = "inline-block";
+  delAddBtnLang++;
+  // Show Delete Button
+  if (delAddBtnLang == 1) {
+    delLangBtn.style.display = "inline-block";
   }
 }
 
-// Deleting inputs if user created more then required
-function delLanguageButton() {}
+// Deleting inputs if user created more then requirement
+function delLanguageBtn() {
+  // Removing last Element
+  langDiv.removeChild(langDiv.children[`${delAddBtnLang--}`]);
+  // Hide Delete Button
+  if (delAddBtnLang == 0) {
+    delLangBtn.style.display = "none";
+  }
+}
+
+// Add+ More Input for Expertise
+let ExpertDiv = document.querySelector("#expertise-Div") as HTMLElement;
+let delExpertBtn = document.querySelector("#del-Expert-Btn") as HTMLElement;
+let delAddBtnExpert = 0;
+
+function addExpertBtn() {
+  // Creating HTML Element
+  let addExpeBtn = document.createElement("input");
+  // New Attributes for Created Element
+  addExpeBtn.setAttribute("type", "text");
+  addExpeBtn.setAttribute("id", "expertise-Input");
+  addExpeBtn.classList.add("form-input");
+  addExpeBtn.setAttribute("required", "");
+  addExpeBtn.setAttribute("placeholder", "Enter Here");
+  addExpeBtn.setAttribute("value", "");
+  // Inserting Input
+  ExpertDiv.append(addExpeBtn);
+  // Increment for Managing Delete Button
+  delAddBtnExpert++;
+  // Show Delete Button
+  if (delAddBtnExpert == 1) {
+    delExpertBtn.style.display = "inline-block";
+  }
+}
+
+// Deleting inputs if user created more then requirement
+function delExpertiseBtn() {
+  // Removing last Element
+  ExpertDiv.removeChild(ExpertDiv.children[`${delAddBtnExpert--}`]);
+  // Hide Delete Button
+  if (delAddBtnExpert == 0) {
+    delExpertBtn.style.display = "none";
+  }
+}
